183ef452c6ffed90b505e887235e62081e2c46ae
Igor Pontes <igoppop@gmail.com>
2021-12-12 22:28:58 +0000

    AoC day 12

---
+++ adventofcode2021/day12/1.py
@@ -0,0 +1,39 @@
+counter = 0
+start_caves = []
+caves = [] 
+def main():
+    global start_caves, caves, counter
+    with open('input.txt', 'r') as file:
+        while line := file.readline():
+            temp = start_caves.append(line[:-1].split("-")[0] if line[:-1].split("-")[0] != "start" else line[:-1].split("-")[1]) if line[:-1].split("-")[0] == "start" or line[:-1].split("-")[1] == "start" else caves.append((line[:-1].split("-")[0], "end")) if line[:-1].split("-")[1] == "end" else caves.append((line[:-1].split("-")[1], "end")) if line[:-1].split("-")[0] == "end" else (line[:-1].split("-")[0], line[:-1].split("-")[1])
+            if temp != None:
+                caves.append(temp)
+                caves.append(temp[::-1])
+    for s in start_caves:
+        paths = ["start"]
+        find_paths(s, "start", paths)
+    print(counter) 
+
+def find_paths(current, past, paths):
+    global counter, start_caves, caves
+    if current == "end":
+        paths.append("end")
+        counter += 1
+        return
+    if current.islower():
+        if paths.count(current) < 1:
+            for t in paths:
+                if t[0] == current and t[1].islower() and paths.count(t[1]) > 1:
+                    return 
+            paths.append(current)
+        else:
+            return
+    else:
+        paths.append(current)
+    for c in caves:
+        if c[0] == current:
+            temp = [x for x in paths]
+            find_paths(c[1], current, temp)
+    return 
+
+main()
